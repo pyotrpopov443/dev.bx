4a7fc5817672ba8309a78c3621c7fb7cc146f3be
petr <pyotr.popov443@mail.ru>
2021-10-08 19:01:53 +0200

    index.php: всё вернул на место

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
 
-$socket = fsockopen("test.shelenkov.com", 80, $errorCode, $errorString);
-if (!$socket)
-{
-	echo "$errorCode ($errorString)<br />\n";
-	die();
-}
-
-$result  = "" . "\r\n";
-$result .= "" . "\r\n";
-$result .= "" . "\r\n";
-$result .= "\r\n";
-
-fwrite($socket, $result);
-while (!feof($socket))
-{
-	echo fgets($socket);
-}
-fclose($socket);
+echo "Hello World!";
