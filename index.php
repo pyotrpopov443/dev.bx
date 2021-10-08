62ec4fdfda23803509ad223f21e9e1894b3a3a5d
petr <pyotr.popov443@mail.ru>
2021-10-08 17:55:49 +0200

    index.php: что-то страшное для лекции

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hello, GitHub!';
+$socket = fsockopen("test.shelenkov.com", 80, $errorCode, $errorString);
+if (!$socket)
+{
+	echo "$errorCode ($errorString)<br />\n";
+	die();
+}
+
+$result  = "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "\r\n";
+
+fwrite($socket, $result);
+while (!feof($socket))
+{
+	echo fgets($socket);
+}
+fclose($socket);
