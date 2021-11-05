efdbbf51d8dea3941d78e6b60b378e6e8e1b49ec
petr <pyotr.popov443@mail.ru>
2021-11-05 22:59:16 +0200

    homework-5: delete classwork files

--- php/lib/todos-functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getTodosByStatus(array $todos, bool $status): array
-{
-	return array_filter($todos, function($todo) use ($status){
-		return $todo['completed'] === $status;
-	});
-}
--- php/resources/blocks/_todo.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** @var array $todo */
-?>
-
-<div class="todos-item">
-	<div class="todos-item-checkbox"><?=$todo['completed'] ? "X" : ""?></div>
-	<div class="todos-item-title <?=$todo['completed'] ? "completed" : ""?>"><?=$todo['title']?></div>
-</div>
